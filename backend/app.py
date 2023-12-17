8945a268e218baae4a112e8e7651823a42b176a8
vv01sam <dr.physhio@vv01sam-3.local>
2023-12-17 18:09:26 +0900

    Initial project setup

--- backend/app.py
+++ backend/app.py
@@ -3,3 +3,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+if __name__ == '__main__':
+    app.run(debug=True)
+    @app.route('/api/data')
+def data():
+    return {'key': 'value'}
---
+++ frontend/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log("Frontend is loaded and ready.");
+});
+console.log("Frontend JavaScript is working!");
+// script.js に追加
+function updateContent() {
+    // 内容を更新するための関数
+  }
+  
